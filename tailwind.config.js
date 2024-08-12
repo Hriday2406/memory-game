5a711c3a406e9a7f0da77b2f1faf095e87111f2b
Hriday2406 <78265700+Hriday2406@users.noreply.github.com>
2024-08-13 02:47:45 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        fontFamily: {
+            display: ['"Dancing Script"', "cursive"],
+            body: ['"Roboto"', "sans-serif"],
+        },
+        extend: {},
+    },
+    plugins: [],
+};
+
