07182610ab781ddca399683eccbbb47fbb054b8e
Hriday2406 <78265700+Hriday2406@users.noreply.github.com>
2024-09-20 00:59:02 +0530

    Add interactions to info btn

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -5,21 +5,21 @@ export default function Info() {
   const [visible, setVisible] = useState(true);
   return (
     <>
-      <div className="info flex flex-col absolute bottom-8 right-20">
+      <div className="absolute flex flex-col info bottom-8 right-20">
         <div
           className={`infoContainer flex flex-col gap-2 -translate-x-12 select-none ${
             visible && "hidden"
           } `}
         >
-          <p className="bg-white text-black text-center p-2 px-4 rounded-2xl">
+          <p className="p-2 px-4 text-center text-black bg-white rounded-2xl">
             Don't click on the same card twice!
           </p>
-          <p className="bg-white text-black text-center p-2 px-4 rounded-2xl">
+          <p className="p-2 px-4 text-center text-black bg-white rounded-2xl">
             Click on the Anime Memory Game logo to go back.
           </p>
         </div>
         <InfoCircleOutlined
-          className="text-5xl self-end"
+          className="self-end text-5xl transition-all hover:scale-110"
           onClick={() => {
             let temp = visible;
             setVisible(!temp);
